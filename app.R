38477d01b411ba93d2f2240faa8bad616bcf0417
amrrs <amrrs@outlook.com>
2017-09-20 20:17:12 +0530

    Initial version of the code

---
+++ app.R
@@ -0,0 +1,23 @@
+
+
+shinyApp(
+  
+  ui = shinyUI(htmlTemplate("template1.html")),
+  
+  server = shinyServer(function(input, output) {
+    
+           data1 <- data.frame(iris)
+           
+           output$title <- renderText('Shiny Custom HTML Template')
+           
+           output$summary <- renderPrint(summary(data1))
+           
+           output$plot1 <- renderPlot(plot(data1[,c(2,3)]))
+           
+           output$plot2 <- renderPlot(plot(data1[,c(3,4)]))
+           
+           
+           
+  })
+  
+)
