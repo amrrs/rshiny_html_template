0c60a4cec8f481396ca3ebc45b65d6fc4a1fcfb4
mikael04 <mikael.coletto.eng@gmail.com>
2023-08-15 06:18:27 -0300

    carousel v1;

--- app.R
+++ app.R
@@ -1,23 +1,81 @@
 library(shiny)
+library(shiny.router)
+library(bslib)
 
-shinyApp(
+home_page <- div(
+  class = "home",
+  titlePanel("Home page"),
+  p("This is the home page!"),
+  uiOutput("power_of_input"),
   
-  ui = shinyUI(htmlTemplate("template1.html")),
-  
-  server = shinyServer(function(input, output) {
-    
-           data1 <- data.frame(iris)
-           
-           output$title <- renderText('Shiny Custom HTML Template')
-           
-           output$summary <- renderPrint(summary(data1))
-           
-           output$plot1 <- renderPlot(plot(data1[,c(2,3)]))
-           
-           output$plot2 <- renderPlot(plot(data1[,c(3,4)]))
-           
-           
-           
+  img(src="www/logo-navbar-aps.png"),
+)
+
+painel <- div(
+  class="panel",
+  titlePanel("PAINEL"),
+  p("This is the another page!"),
+)
+
+menu <- 
+  tags$nav(
+    class = "navbar navbar-expand-lg navbar-light navbar-style",
+    tags$div(
+      id="navbarSupportedContent",
+      class = "collapse navbar-collapse justify-content-end",
+      tags$ul(
+      class = "navbar-nav navbar-font",
+        tags$li(class = "nav-item", a(class = "nav-link", href = route_link("/"), "SOBRE")),
+        tags$li(class = "nav-item", a(class = "nav-link", href = route_link("painel"), "PAINEL")),
+      )
+    )
+  )
+footer <- 
+  tags$div(
+    class="card-footer footer-style text-muted fixed-bottom",
+    img(src="www/logo-navbar-aps.png"),
+    tags$img(src="www/logo-navbar-aps.png"),
+    img(src="www/logo-navbar-aps.svg"),
+    tags$img(src="www/logo-navbar-aps.svg")
+  )
+  # 
+  # HTML('<div class="card-footer footer-style text-muted fixed-bottom">
+  #         <img src="img/logos-footer.png" alt="Logo dos parceiros"></img>
+  #       </div>')
+
+
+ui <- fluidPage(
+  includeCSS("css/style.css"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css"),
+  ),
+  theme = bslib::bs_theme(version = 5),
+  menu,
+  tags$hr(),
+  tags$hr(),
+  router_ui(
+    route("/", home_page),
+    route("painel", painel)
+  ),
+  footer
+)
+
+server <- function(input, output, session) {
+  router_server()
+  component <- reactive({
+    if (is.null(get_query_param()$add)) {
+      return(0)
+    }
+    as.numeric(get_query_param()$add)
   })
   
-)
+  output$power_of_input <- renderUI({
+    HTML(paste(
+      "I display input increased by <code>add</code> GET parameter from app url and pass result to <code>output$power_of_input</code>: ",
+      as.numeric(input$int) + component()))
+  })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 4
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
